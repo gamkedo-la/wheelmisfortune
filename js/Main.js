f8c2bc1d0026c4db1f399c12f46a71c9536e223b
Cameron Button <csbutton01@hotmail.com>
2017-12-08 10:41:10 -0700

    Revert "healthbar"
    This reverts commit 934ae2fb0f2889808d73e721407c1f20e7824d62, reversing
    changes made to efe31103d7058123b804c6b531de54152551a698.

--- js/Main.js
+++ js/Main.js
@@ -1,58 +1,26 @@
 // save the canvas for dimensions, and its 2d context for drawing to it
 const PLAYER_STARTING_HEALTH = 3;
 var canvas, canvasContext;
-var scaledCanvas, scaledContext;
-
 var gameRunning = true;
 var animationFrameNumber;
 var playerHealthArray = [];
 
-var player;
-
-const PIXEL_SCALE_UP = 3; // Number of times to scale up art tiles
-
-
+var player = new Player(400, 400);
 
 function calculateMousePos(evt) {
-	var rect = scaledCanvas.getBoundingClientRect(),
+	var rect = canvas.getBoundingClientRect(),
 		root = document.documentElement;
 
 	// account for the margins, canvas position on page, scroll amount, etc.
 	mouseX = evt.clientX - rect.left;
 	mouseY = evt.clientY - rect.top;
-	var canvasStretch = scaledCanvas.width/ canvas.width;
-	mouseX /= canvasStretch;
-	mouseY /= canvasStretch;
 }
 
 window.onload = function() {
 	canvas = document.getElementById("gameCanvas");
 	canvasContext = canvas.getContext("2d");
-
-		// Get references for gameCanvas
-    scaledCanvas = document.getElementById('gameCanvas');
-    canvas = document.createElement('canvas');
-
-    // Size gameCanvas
-	canvas.width = 960/ PIXEL_SCALE_UP;
-	canvas.height = 540/ PIXEL_SCALE_UP;
-	scaledCanvas.width =960;
-	scaledCanvas.height = 540;
-
-	canvasContext = canvas.getContext('2d');
-	scaledContext = scaledCanvas.getContext('2d');
-	scaledContext.fillStyle = "black";
-
-	// Helps it not blur from the scaling:
-	canvasContext.mozImageSmoothingEnabled = false;
-	canvasContext.imageSmoothingEnabled = false;
-	canvasContext.msImageSmoothingEnabled = false;
-	canvasContext.webkitImageSmoothingEnabled = false;
-	scaledContext.mozImageSmoothingEnabled = false;
-	scaledContext.imageSmoothingEnabled = false;
-	scaledContext.msImageSmoothingEnabled = false;
-	scaledContext.webkitImageSmoothingEnabled = false;
-
+	canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 	loadImages();
 };
 
@@ -62,7 +30,7 @@ function loadingDoneSoStartGame() {
 
 	animationFrameNumber = requestAnimationFrame(gameController.update);
 
-	scaledCanvas.addEventListener("mousemove", calculateMousePos);
+	canvas.addEventListener("mousemove", calculateMousePos);
 
 	document.addEventListener("keydown", keyPressed);
 	document.addEventListener("keyup", keyReleased);
@@ -77,16 +45,10 @@ function loadingDoneSoStartGame() {
 	window.addEventListener("focus", windowOnFocus);
 	window.addEventListener("blur", windowOnBlur);
 
-	/* // commenting out until/unless we can unblurry Chrome
-	window.addEventListener("resize", onResize);
-    onResize();*/
-
 	//Disable right click context menu
 	document.oncontextmenu = function() {
 		return false;
 	};
-
-	player = new Player(400, 400);
 } //end of loadingDoneSoStartGame
 
 function windowOnFocus() {
@@ -101,18 +63,10 @@ function windowOnBlur() {
 	cancelAnimationFrame(animationFrameNumber);
 }
 
-function onResize() { // changing window dimensions
-    if (!canvas) return;
-    var gameRatio = canvas.height/canvas.width;
-    var widthIfHeightScaled = window.innerHeight / gameRatio;
-    if(widthIfHeightScaled <= window.innerWidth) {
-        scaledCanvas.width = widthIfHeightScaled;
-        scaledCanvas.height = window.innerHeight;
-    } else {
-        var heightIfWidthScaled = window.innerWidth * gameRatio;
-        scaledCanvas.width = window.innerWidth;
-        scaledCanvas.height = heightIfWidthScaled;
-    }
+function handleInput(){
+	if (key_Space || mouse_Left){
+		fireShot();
+	}
 }
 
 function moveEverything() {
@@ -121,8 +75,16 @@ function moveEverything() {
 		return; // skipping gamemovement while wheelShowing
 	}
 	player.move();
+	for (var i = 0; i < shotList.length; i++) {
+		shotList[i].move();
+	}
 	moveEnemies();
-	moveShots();
+	for (var r = shotList.length - 1; r >= 0; r--) {
+		if (shotList[r].removeMe) {
+			shotList.splice(r, 1);
+		}
+	}
+
 } //end of moveEverything
 
 function drawEverything() {
@@ -131,6 +93,7 @@ function drawEverything() {
 	canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 	canvasContext.drawImage(backGroundPic,0,0,canvas.width,canvas.height);
 	player.draw();
+
 
 	for (var i = 0; i < shotList.length; i++) {
 		shotList[i].draw();
@@ -147,11 +110,6 @@ function drawEverything() {
 		drawWheel();
 	}
 
-
-	scaledContext.drawImage(canvas, 0, 0, canvas.width, canvas.height,
-		0, 0, scaledCanvas.width, scaledCanvas.height);
-
-
 	playerHealthArray.length = player.playerHealth;
 	for (var i = 0; i < playerHealthArray.length; i++) {
 		playerHealthArray[i] = true;
@@ -162,11 +120,31 @@ function drawEverything() {
 		}
 	}
 
-	if(debug) {
-		frameCounter.getFps();
-	}
+
+	frameCounter.getFps();
 } // end of drawEverything
 
 function collideEverything() {
-	checkBulletCollisions();
+	var distX;
+	var distY;
+	for (var i = 0; i < shotList.length; i++) {
+		var currentShot = shotList[i]
+
+		for (var j = 0; j < enemyList.length; j++) {
+			var currentEnemy = enemyList[j];
+
+			//Hacky collision code, replace at some point
+			distX = currentShot.x - currentEnemy.x;
+			distY = currentShot.y - currentEnemy.y;
+			if ((distX*distX + distY*distY) <= 100) {
+				currentShot.removeMe = true;
+				currentEnemy.life -= currentShot.damage;
+				if (currentEnemy.life <= 0) {
+					currentEnemy.remove = true;
+					currentEnemy.life = 9999999; //So it won't keep adding more enemies before it's removed.
+					enemyList.push(new TestEnemy(200, 200));
+				}
+			}
+		}
+	}
 } //end of collideEverything
